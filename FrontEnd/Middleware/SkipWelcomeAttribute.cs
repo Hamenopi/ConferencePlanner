276da6c1b96ac4da4ba8c6f713cefa4a991944ed
Justin Hamilton <JHamilton@pas.com>
2020-09-11 16:31:50 -0500

    Before Add personal agenda

---
+++ FrontEnd/Middleware/SkipWelcomeAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FrontEnd
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class SkipWelcomeAttribute : Attribute
+    {
+
+    }
+}
+
